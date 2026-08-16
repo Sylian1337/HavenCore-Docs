0ee490a11367171162e20d522a5f487bf0b992ca
Morten <selex911@gmail.com>
2026-08-16 22:45:41 +0200

    Change GitHub organization name to Sylian1337

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'HavenCore', // Usually your GitHub org/user name.
+  organizationName: 'Sylian1337', // Usually your GitHub org/user name.
   projectName: 'HavenCore-Docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
